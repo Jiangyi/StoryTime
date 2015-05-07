b762659157ad81275616b465a41de7e69fd247c9
Andrew Jiang <sam.andrew.jiang@gmail.com>
2015-05-07 19:06:27 -0400

    Add custom keylayout options
    If the specified custom key layout does not exist, the default will be used.
    Custom key settings will also persist through shutdowns.
    
    For now, press "+" to load the custom keylayout, press "-" to clear to defaults.
    Will fix when we get a proper UI going.

--- core/src/com/jxz/notcontra/handlers/InputManager.java
+++ core/src/com/jxz/notcontra/handlers/InputManager.java
@@ -3,25 +3,27 @@ package com.jxz.notcontra.handlers;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.jxz.notcontra.entity.*;
 import com.jxz.notcontra.game.Game;
-import com.jxz.notcontra.menu.ScreenshotFactory;
 import com.jxz.notcontra.states.LoadState;
 import com.jxz.notcontra.states.PauseState;
 import com.jxz.notcontra.states.PlayState;
 
-import java.nio.ByteBuffer;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 /**
  * Created by Samuel on 2015-03-27.
  */
 public class InputManager implements InputProcessor {
 
+    private Preferences keyPreferences = Gdx.app.getPreferences("InputManager");
     private Game game;
     private GameStateManager gsm;
     private static InputManager manager;
@@ -51,51 +53,60 @@ public class InputManager implements InputProcessor {
     public boolean keyDown(int keycode) {
         // Movement controls only operational if in play state
         if (gsm.getCurrentState() instanceof PlayState) {
+
+            // Load saved key preferences
+            // TODO: Remove this when we get a proper key config UI
+            if (keycode == Input.Keys.PLUS) {
+                setSavedKeyPreferences();
+                return true;
+            }
+
+            // Clear all saved key preferences
+            if (keycode == Input.Keys.MINUS) {
+                keyPreferences.clear();
+                return true;
+            }
+
             player = gsm.getPlayState().getPlayer();
             if (player.isAlive()) {
                 if (!player.isRooted()) {
                     // Update sprinting state
-                    if (keycode == Input.Keys.SHIFT_LEFT && player.getJumpState() == 0) {
+                    if (keycode == keyPreferences.getInteger("sprint", Input.Keys.SHIFT_LEFT) && player.getJumpState() == 0) {
                         player.setSprinting(true);
                         return true;
                     }
 
                     // Standard WASD Movement
-                    if (keycode == Input.Keys.A) {
+                    if (keycode == keyPreferences.getInteger("left", Input.Keys.A)) {
                         player.getMovementState().add(-1, 0);
                     }
-                    if (keycode == Input.Keys.D) {
+                    if (keycode == keyPreferences.getInteger("right", Input.Keys.D)) {
                         player.getMovementState().add(1, 0);
                     }
-                    if (keycode == Input.Keys.W) {
+                    if (keycode == keyPreferences.getInteger("up", Input.Keys.W)) {
                         player.getMovementState().add(0, 1);
                     }
-                    if (keycode == Input.Keys.S) {
+                    if (keycode == keyPreferences.getInteger("down", Input.Keys.S)) {
                         player.getMovementState().add(0, -1);
                     }
 
-                    // Jump if max jumps is not reached
-                    if (keycode == Input.Keys.SPACE && !player.isJumping()) {
-                        player.jump();
-                    }
                 }
 
                 // Jump if max jumps is not reached
-                if (keycode == Input.Keys.SPACE && !player.isJumping()) {
+                if (keycode == keyPreferences.getInteger("jump", Input.Keys.SPACE) && !player.isJumping()) {
                     player.jump();
                 }
-
                 // Attack | cast keys
-                if (keycode == Input.Keys.H) {
+                if (keycode == keyPreferences.getInteger("melee", Input.Keys.H)) {
                     player.getSkills().setActive(0, true);
                 }
 
-                if (keycode == Input.Keys.J) {
+                if (keycode == keyPreferences.getInteger("rangeAttack", Input.Keys.J)) {
                     player.getSkills().setActive(1, true);
                 }
 
                 // K has become the "piss off everything on the map" button
-                if (keycode == Input.Keys.K) {
+                if (keycode == keyPreferences.getInteger("aggroAll", Input.Keys.K)) {
                     for (Entity e : EntityManager.getInstance().getEntitiesList()) {
                         if (e instanceof Monster) {
                             Monster m = (Monster) e;
@@ -104,7 +115,7 @@ public class InputManager implements InputProcessor {
                         }
                     }
                 }
-                if (keycode == Input.Keys.L) {
+                if (keycode == keyPreferences.getInteger("spawnMonster", Input.Keys.L)) {
                     // Spawn some slimes
                     Slime slime = (Slime) EntityFactory.spawn(Slime.class, getCursorInWorld().x, getCursorInWorld().y);
                     slime.init();
@@ -113,18 +124,18 @@ public class InputManager implements InputProcessor {
                 }
 
                 // Debug Mode
-                if (keycode == Input.Keys.F) {
+                if (keycode == keyPreferences.getInteger("setDebug", Input.Keys.F)) {
                     Game.setDebugMode(!Game.getDebugMode());
                 }
 
                 // Interact key
-                if (keycode == Input.Keys.E) {
+                if (keycode == keyPreferences.getInteger("interact", Input.Keys.E)) {
                     player.interact();
                 }
             }
 
             // PLAY STATE SWITCH STATE TEST
-            if (keycode == Input.Keys.ESCAPE) {
+            if (keycode == keyPreferences.getInteger("escape", Input.Keys.ESCAPE)) {
                 TextureRegion background = ScreenUtils.getFrameBufferTexture(game.getViewport().getScreenX(), game.getViewport().getScreenY(), game.getViewport().getScreenWidth(), game.getViewport().getScreenHeight());
                 gsm.setState(GameStateManager.State.PAUSE);
                 gsm.getPauseState().setBackground(background);
@@ -133,7 +144,7 @@ public class InputManager implements InputProcessor {
         }
         // PAUSE STATE SWITCH STATE TEST
         if (gsm.getCurrentState() instanceof PauseState) {
-            if (keycode == Input.Keys.ESCAPE) {
+            if (keycode == keyPreferences.getInteger("escape", Input.Keys.ESCAPE)) {
                 gsm.setState(GameStateManager.State.PLAY);
                 player.updateMovementState();
                 return true;
@@ -141,7 +152,7 @@ public class InputManager implements InputProcessor {
         }
         // LOAD STATE SWITCH STATE TEST
         if (gsm.getCurrentState() instanceof LoadState) {
-            if (keycode == Input.Keys.ESCAPE) {
+            if (keycode == keyPreferences.getInteger("escape", Input.Keys.ESCAPE)) {
                 gsm.setState(GameStateManager.State.PLAY);
                 return true;
             }
@@ -160,21 +171,21 @@ public class InputManager implements InputProcessor {
             return true;
         }
 
-        if (keycode == Input.Keys.PAGE_UP) {
+        if (keycode == keyPreferences.getInteger("volUp", Input.Keys.PAGE_UP)) {
             AudioHelper.setMusicVolume(AudioHelper.getMusicVolume() + 0.1f);
             return true;
         }
 
-        if (keycode == Input.Keys.PAGE_DOWN) {
+        if (keycode == keyPreferences.getInteger("volDown", Input.Keys.PAGE_DOWN)) {
             AudioHelper.setMusicVolume(AudioHelper.getMusicVolume() - 0.1f);
             return true;
         }
 
-        if (keycode == Input.Keys.ALT_LEFT) {
+        if (keycode == keyPreferences.getInteger("camZoomOut", Input.Keys.ALT_LEFT)) {
             game.getPlayerCam().zoom += 1;
         }
 
-        if (keycode == Input.Keys.ALT_RIGHT) {
+        if (keycode == keyPreferences.getInteger("camZoomIn", Input.Keys.ALT_RIGHT)) {
             game.getPlayerCam().zoom -= 1;
         }
         return false;
@@ -188,40 +199,36 @@ public class InputManager implements InputProcessor {
             // Released keys signal end of movement if player is not rooted
             if (player.isAlive()) {
                 if (!player.isRooted()) {
-                    if (keycode == Input.Keys.SHIFT_LEFT) {
+                    if (keycode == keyPreferences.getInteger("sprint", Input.Keys.SHIFT_LEFT)) {
                         player.setSprinting(false);
                     }
 
-                    if (keycode == Input.Keys.A) {
+                    // Standard WASD Movement
+                    if (keycode == keyPreferences.getInteger("left", Input.Keys.A)) {
                         player.getMovementState().add(1, 0);
                     }
-                    if (keycode == Input.Keys.D) {
+                    if (keycode == keyPreferences.getInteger("right", Input.Keys.D)) {
                         player.getMovementState().add(-1, 0);
                     }
-                    if (keycode == Input.Keys.W) {
+                    if (keycode == keyPreferences.getInteger("up", Input.Keys.W)) {
                         player.getMovementState().add(0, -1);
                     }
-                    if (keycode == Input.Keys.S) {
+                    if (keycode == keyPreferences.getInteger("down", Input.Keys.S)) {
                         player.getMovementState().add(0, 1);
                     }
                 }
 
                 // Resets jump flag if space bar is released - ready to jump again
-                if (keycode == Input.Keys.SPACE) {
+                if (keycode == keyPreferences.getInteger("jump", Input.Keys.SPACE)) {
                     player.setIsJumping(false);
                 }
                 // Release active skills
-                if (keycode == Input.Keys.H) {
+                if (keycode == keyPreferences.getInteger("melee", Input.Keys.H)) {
                     player.getSkills().setActive(0, false);
                 }
 
-                if (keycode == Input.Keys.J) {
+                if (keycode == keyPreferences.getInteger("rangeAttack", Input.Keys.J)) {
                     player.getSkills().setActive(1, false);
-                }
-
-                // Resets jump flag if space bar is released - ready to jump again
-                if (keycode == Input.Keys.SPACE) {
-                    player.setIsJumping(false);
                 }
             }
         }
@@ -275,5 +282,30 @@ public class InputManager implements InputProcessor {
 
     public Vector2 getCursorDirection(Vector2 relativePos) {
         return getCursorInWorld().sub(relativePos);
+    }
+
+    private boolean setSavedKeyPreferences() {
+        String FILENAME = "keys/SavedLayout.txt";
+        FileHandle fileHandle = Gdx.files.internal(FILENAME);
+        BufferedReader br = new BufferedReader(fileHandle.reader());
+
+        String line;
+        String[] tmp;
+        try {
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                // Only parse uncommented lines
+                if (!line.trim().startsWith("#") && line.trim().length() > 0) {
+                    // Regex for whitespace and tabs
+                    tmp = line.split("\\s+");
+                    keyPreferences.putInteger(tmp[0], Input.Keys.valueOf(tmp[1]));
+                }
+            }
+            keyPreferences.flush();
+            return true;
+        } catch (IOException e) {
+            System.out.println(FILENAME + "does not exist!");
+            return false;
+        }
     }
 }
